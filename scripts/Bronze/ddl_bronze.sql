32abe43cc31b7a8b86dc45eca6decf5299101d10
Nithin kadam <158561219+Nithin-Creato@users.noreply.github.com>
2025-11-20 16:50:46 +0530

    Update ddl_bronze.sql

--- scripts/Bronze/ddl_bronze.sql
+++ scripts/Bronze/ddl_bronze.sql
@@ -1,130 +1,66 @@
-create or alter procedure bronze.load_bronze as
-begin
-	declare @start_time Datetime,@End_time Datetime, @batch_start_time Datetime, @batch_end_time Datetime;
-	begin try 
-		set @batch_start_time = GETDATE();
-		print '============================================';
-		print 'Loading the Bronze Layer';
-		print '============================================';
+--- create tables
+create table bronze.crm_cust_info(
+cst_id int,
+cst_key nvarchar(50),
+cst_firstname nvarchar(50),
+cst_lastname nvarchar(50),
+cst_marital_status nvarchar(50),
+cst_gndr nvarchar(50),
+cst_create_date date
+);
 
-		print '---------------------------------------------';
-		print 'Loading CRM Tables';
-		print '---------------------------------------------';
+create table bronze.crm_prd_info(
+prd_id int,
+prd_key nvarchar(50),
+prd_nm nvarchar(50),
+prd_cost nvarchar(50),
+prd_line nvarchar(50),
+cst_gndr nvarchar(50),
+prd_start_dt date,
+prd_end_dt date
 
-		set @start_time = GETDATE();
-		print '>> Truncating table: bronze.crm_cust_info ';
-		truncate table bronze.crm_cust_info;
-		print '>> Inserting table: bronze.crm_cust_info ';
-		bulk insert bronze.crm_cust_info
-		from 'C:\Users\nithi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.CSV'
-		with 
-		( 
-			firstrow = 2 ,
-			fieldterminator = ',',
-			tablock 
-		);
-		set @End_time = GETDATE();
-		print '>> Load Duration:' + cast(datediff( second, @start_time, @End_time ) as nvarchar) + ' seconds';
-		print '------------------------------------------------------------';
+);
 
 
-		set @start_time = GETDATE();
-		print '>> Truncating table: bronze.crm_prd_info';
-		truncate table bronze.crm_prd_info;
-		print '>> Inserting table: bronze.crm_prd_info';
-		bulk insert bronze.crm_prd_info
-		from 'C:\Users\nithi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.CSV'
-		with 
-		( 
-			firstrow = 2 ,
-			fieldterminator = ',',
-			tablock 
-		);
-		set @End_time = GETDATE();
-		print '>> Load Duration:' + cast(datediff( second, @start_time, @End_time ) as nvarchar) + ' seconds';
-		print '------------------------------------------------------------';
+create table bronze.crm_prd_info(
+prd_id int,
+prd_key nvarchar(50),
+prd_nm nvarchar(50),
+prd_cost int,
+prd_line nvarchar(50),
+cst_gndr nvarchar(50),
+prd_start_dt datetime,
+prd_end_dt datetime
 
-		set @start_time = GETDATE();
-		print '>> Truncating table: bronze.crm_sales_details';
-		truncate table bronze.crm_sales_details;
-		print '>> Inserting table: bronze.crm_sales_details';
-		bulk insert bronze.crm_sales_details
-		from 'C:\Users\nithi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.CSV'
-		with 
-		( 
-			firstrow = 2 ,
-			fieldterminator = ',',
-			tablock 
-		);
-		set @End_time = GETDATE();
-		print '>> Load Duration:' + cast(datediff( second, @start_time, @End_time ) as nvarchar) + ' seconds';
-		print '------------------------------------------------------------';
+);
 
-		print '---------------------------------------------';
-		print 'Loading ERP Tables';
-		print '---------------------------------------------';
+create table bronze.crm_sales_details(
+sls_ord_num nvarchar(50),
+sls_prd_key nvarchar(50),
+sls_cust_id int,
+sls_order_dt int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
+sls_quantity int,
+sls_price int
 
-		set @start_time = GETDATE();
-		print '>> Truncating table: bronze.erp_cust_az12';
-		truncate table bronze.erp_cust_az12;
-		print '>> Inserrting table: bronze.erp_cust_az12';
-		bulk insert bronze.erp_cust_az12
-		from 'C:\Users\nithi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.CSV'
-		with 
-		( 
-			firstrow = 2 ,
-			fieldterminator = ',',
-			tablock 
-		);
-		set @End_time = GETDATE();
-		print '>> Load Duration:' + cast(datediff( second, @start_time, @End_time ) as nvarchar) + ' seconds';
-		print '------------------------------------------------------------';
+);
 
+create table bronze.erp_cust_az12(
+CID nvarchar(50),
+BDATE date,
+GEN nvarchar(50)
+)
 
-		set @start_time = GETDATE();
-		print '>> Truncating table: bronze.erp_loc_a101';
-		truncate table bronze.erp_loc_a101;
-		print '>> Inserting table: bronze.erp_loc_a101';
-		bulk insert bronze.erp_loc_a101
-		from 'C:\Users\nithi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.CSV'
-		with 
-		( 
-			firstrow = 2 ,
-			fieldterminator = ',',
-			tablock 
-		);
-		set @End_time = GETDATE();
-		print '>> Load Duration:' + cast(datediff( second, @start_time, @End_time ) as nvarchar) + ' seconds';
-		print '------------------------------------------------------------';
+create table bronze.erp_loc_a101(
+CID nvarchar(50),
+CNTRY nvarchar(50)
+)
 
-
-		set @start_time = GETDATE();
-		print '>> Truncating table: bronze.erp_PX_CAT_G1V2';
-		truncate table bronze.erp_PX_CAT_G1V2;
-		print '>> Inserting table: bronze.erp_PX_CAT_G1V2';
-		bulk insert bronze.erp_PX_CAT_G1V2
-		from 'C:\Users\nithi\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.CSV'
-		with 
-		( 
-			firstrow = 2 ,
-			fieldterminator = ',',
-			tablock 
-		);
-		set @End_time = GETDATE();
-		print '>> Load Duration:' + cast(datediff( second, @start_time, @End_time ) as nvarchar) + ' seconds';
-		print '------------------------------------------------------------';
-
-		set @batch_end_time = GETDATE();
-		print '>> Load Duration:' + cast(datediff( second, @batch_start_time, @batch_end_time  ) as nvarchar) + ' seconds';
-		print '------------------------------------------------------------';
-
-	end try 
-	begin catch 
-		print '============================================';
-		print 'Error occured during loading bronze layer';
-		print 'Error Message' + Error_Message();
-		print 'Error Message' + cast(Error_number() as nvarchar);
-		print 'Error Message' + cast(Error_state() as nvarchar);
-		print '============================================';
-	end catch
-end 
+create table bronze.erp_PX_CAT_G1V2(
+ID nvarchar(50),
+CAT nvarchar(50),
+SUBCAT nvarchar(50),
+MAINTENANCE nvarchar(50)
+)
